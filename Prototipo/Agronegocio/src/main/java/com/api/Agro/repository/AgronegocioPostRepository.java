9ccf0aee9122b6cecf7b8e4a27d93b472650e6e9
Renan Miranda Zanella <82384561+Renanmz@users.noreply.github.com>
2025-05-13 21:46:25 -0300

    Add files via upload

---
+++ Prototipo/Agronegocio/src/main/java/com/api/Agro/repository/AgronegocioPostRepository.java
@@ -0,0 +1,9 @@
+package com.api.Agro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.Agro.model.AgronegocioPostModel;
+
+public interface AgronegocioPostRepository extends JpaRepository<AgronegocioPostModel, Long>{
+
+}
---
+++ Prototipo/Agronegocio/src/main/java/com/api/Agro/utils/DataUtils.java
@@ -0,0 +1,68 @@
+package com.api.Agro.utils;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.api.Agro.model.AgronegocioPostModel;
+import com.api.Agro.repository.AgronegocioPostRepository;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class DataUtils {
+	
+	@Autowired // para ser o constructor da classe
+	AgronegocioPostRepository agronegociorepository;
+	
+	//@PostConstruct
+		public void savePosts() {
+			
+			List<AgronegocioPostModel> postList = new ArrayList<>();
+			AgronegocioPostModel post1 = new AgronegocioPostModel();
+			
+			post1.setNomefazenda("Alquioda Farm");
+			post1.setCidade("Stardew Valley");
+			post1.setDoenca("Ergostismo");
+			post1.setEstacao("Outono");
+			post1.setRegiao("Brasil, Sudeste, São Paulo");
+			post1.setClima("Nublado");
+			post1.setData(LocalDate.now());
+			post1.setDescricao("Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+	        		+ " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+	        		+ "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+	        		+ "It has survived not only five centuries, but also the leap into electronic typesetting, "
+	        		+ "remaining essentially unchanged. It was popularised in the 1960s with the release of"
+	        		+ " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
+	        		+ "publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+			
+			AgronegocioPostModel post2 = new AgronegocioPostModel();
+			
+			post2.setNomefazenda("Paloma Farm");
+			post2.setCidade("Stardew Valley");
+			post2.setDoenca("Oídio");
+			post2.setEstacao("Primavera");
+			post2.setRegiao("Brasil, Sudeste, Rio de Janeiro");
+			post2.setClima("Ensolarado");
+			post2.setData(LocalDate.now());
+			post2.setDescricao("Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+	        		+ " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+	        		+ "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+	        		+ "It has survived not only five centuries, but also the leap into electronic typesetting, "
+	        		+ "remaining essentially unchanged. It was popularised in the 1960s with the release of"
+	        		+ " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
+	        		+ "publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+			postList.add(post1);
+	        postList.add(post2);
+	        
+	        for(AgronegocioPostModel post: postList){
+	        	AgronegocioPostModel postSaved = agronegociorepository.save(post);
+	            System.out.println(postSaved.getId());
+	        }
+		}
+
+}
