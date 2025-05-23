0c02db8d2de0ee3085496c8667d6d32049deb60b
Renan Miranda Zanella <82384561+Renanmz@users.noreply.github.com>
2025-05-23 19:29:48 -0300

    Delete Agro directory

--- Agro/src/main/java/com/api/Agro/dtos/FazendaRecordDto.java
+++
@@ -1,7 +0,0 @@
-package com.api.Agro.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record FazendaRecordDto(@NotBlank String nomefazenda, @NotBlank String cidade, @NotBlank String regiao) {
-
-}
--- Agro/src/main/java/com/api/Agro/repository/DoencaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.Agro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.Agro.model.DoencaModel;
-
-public interface DoencaRepository extends JpaRepository<DoencaModel, Long> {
-
-}
