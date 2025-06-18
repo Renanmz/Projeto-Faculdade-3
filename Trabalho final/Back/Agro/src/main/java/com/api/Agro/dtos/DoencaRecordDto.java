6ad90fbb066886664406828df25b0ac639682d45
Renan Miranda Zanella <82384561+Renanmz@users.noreply.github.com>
2025-06-17 21:08:49 -0300

    Delete Trabalho final/Back/Agro directory

--- Trabalho final/Back/Agro/src/main/java/com/api/Agro/dtos/DoencaRecordDto.java
+++
@@ -1,7 +0,0 @@
-package com.api.Agro.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DoencaRecordDto(@NotBlank String nome, @NotBlank String tipo, @NotBlank String descricao) {
-
-}
